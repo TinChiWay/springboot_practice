977daa66f2bed12994a59304e2fafc47e7e8170c
TinChiWay <hebeitianzhiwei@outlook.com>
2017-12-12 20:47:05 +0800

    Initial commit

---
+++ src/main/java/edu/tinchiway/controller/GirlController.java
@@ -0,0 +1,33 @@
+package edu.tinchiway.controller;
+
+import com.alibaba.fastjson.JSON;
+import edu.tinchiway.entity.GirlProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+/**
+ * Created by TinChiWay on 2017/12/5.
+ */
+@RestController //@Controller+@ResponseBody 返回json数据格式的控制器
+@RequestMapping("/hello")
+public class GirlController {
+
+    @Autowired
+    private GirlProperties girlProperties;
+
+    /**
+     * @PathVariable 获取url中数据
+     * @RequestParam 获取请求参数的值
+     * @GetMapping 组合注解
+     */
+
+    //@RequestMapping(value = "/say", method = RequestMethod.GET)
+    @GetMapping(value = "/say")//组合注解
+    public String say(@RequestParam(value = "id", required = false, defaultValue = "200") Integer id, @RequestParam("name") String name) {
+        //使用@PathVariable("id") 获取/say/{id}
+        return JSON.toJSONString(girlProperties) + id + name;
+    }
+
+
+}
---
+++ src/main/java/edu/tinchiway/entity/GirlProperties.java
@@ -0,0 +1,40 @@
+package edu.tinchiway.entity;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * Created by TinChiWay on 2017/12/12.
+ */
+
+@Component
+@ConfigurationProperties(prefix = "girl")
+public class GirlProperties {
+    private String cupSize;
+    private Integer age;
+
+    public String getCupSize() {
+        return cupSize;
+    }
+
+    public void setCupSize(String cupSize) {
+        this.cupSize = cupSize;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "GirlProperties{" +
+                "cupSize='" + cupSize + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
